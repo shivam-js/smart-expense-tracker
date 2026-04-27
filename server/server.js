4e26f61b4d911e237cf114653cd447bab3985919
shivam-js <prajapati2shivam@gmail.com>
2026-04-27 22:00:06 +0530

    fix cors properly

--- server/server.js
+++ server/server.js
@@ -2,30 +2,32 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
-const mongoose = require("mongoose");
-
-dotenv.config();
 
 const expenseRoutes = require("./routes/expenseRoutes");
 const authRoutes = require("./routes/authRoutes");
 
-// Connect Database
+dotenv.config();
 connectDB();
 
 const app = express();
 
-// Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
 app.use(express.json());
 
-// External Addition (Routes)---
-app.use("/api/auth", authRoutes);
-app.use("/api/expenses", expenseRoutes);
-
-// Test route
 app.get("/", (req, res) => {
   res.send("Expense Tracker API is running...");
 });
+
+app.use("/api/auth", authRoutes);
+app.use("/api/expenses", expenseRoutes);
 
 const PORT = process.env.PORT || 5000;
 
