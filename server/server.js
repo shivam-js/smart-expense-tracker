f453c61a9b8b7fd5e5c0a51e3f610f1107f35343
shivam-js <prajapati2shivam@gmail.com>
2026-04-27 22:12:59 +0530

    final cors fix

--- server/server.js
+++ server/server.js
@@ -1,36 +1,9 @@
-const express = require("express");
 const cors = require("cors");
-const dotenv = require("dotenv");
-const connectDB = require("./config/db");
 
-const expenseRoutes = require("./routes/expenseRoutes");
-const authRoutes = require("./routes/authRoutes");
-
-dotenv.config();
-connectDB();
-
-const app = express();
-
-app.use(
-  cors({
-    origin: true,
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
-
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Expense Tracker API is running...");
-});
-
-app.use("/api/auth", authRoutes);
-app.use("/api/expenses", expenseRoutes);
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://smart-expense-tracker-kcyv.onrender.com"
+  ],
+  credentials: true
+}));
